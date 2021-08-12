b1edfd643d3a248ee8df4289b4e044ffe1dcd5fc
Hi it's me Anees lub <87905109+aneeslub@users.noreply.github.com>
2021-08-12 06:06:15 +0530

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
   // F this.sendContact(m.chat, '62815158600891', 'Nurutomo', m)
-  this.sendContact(m.chat, '919383400679', 'Toxic alien', m)
+  this.sendContact(m.chat, '917356378949', 'Anees シ♕', m)
   this.sendContact(m.chat, '919567997462', 'Safwan Ganz', m)
 }
 handler.help = ['owner', 'creator']
